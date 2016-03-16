46f9699f4fb3869f241f09f7c47fee3abc015c58
Park Lai <park8lai@gmail.com>
2016-03-16 12:29:23 +0800

    Migrations

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,29 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Applicant::class, function (Faker\Generator $faker) {
+    $courses = Config::get('hook.courses');
+
     return [
         'name' => $faker->name,
         'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
+        'gender' => $faker->randomElement(['M', 'F']),
+        'nationality' => $faker->country,
+        'nric' => $faker->creditCardNumber,
+        'dob' => $faker->date(),
+        'mobile' => $faker->phoneNumber,
+        'address_line_1' => $faker->streetAddress,
+        'address_line_2' => $faker->city,
+        'postal' => $faker->postcode,
+        'address_country' => $faker->country,
+        'english_proficiency' => $faker->randomElement(['None', 'IELTS', 'Other']),
+        'course_type' => $faker->randomElement(['Full time', 'Part time']),
+        'gpa' => $faker->randomFloat(2, 0, 4),
+        'expected_start_date' => $faker->date(),
+        'course_of_interest_1' => $courses['accounting, banking and finance'][array_rand([0,1,2,3,4,5], 1)],
+        'course_of_interest_2' => $courses['business and management'][array_rand([0,1,2,3,4,5], 1)],
+        'course_of_interest_3' => $courses['hospitality and tourism management'][array_rand([0,1,2,3,4,5], 1)],
+        'device_name' => $faker->userName,
+        'is_emailed' => $faker->boolean,
     ];
 });
---
+++ database/migrations/2016_03_08_103056_create_applicants_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applicants', function ($table) {
+            $table->increments('id')->unique();
+            $table->string('name');
+            $table->string('gender')->nullable();
+            $table->string('nationality');
+            $table->string('nric')->nullable();
+            $table->string('dob')->nullable();
+            $table->string('mobile')->nullable();
+            $table->string('email');
+            $table->string('address_line_1')->nullable();
+            $table->string('address_line_2')->nullable();
+            $table->integer('postal')->nullable();
+            $table->string('address_country')->nullable();
+
+            $table->string('english_proficiency');
+            $table->float('gpa')->nullable();
+            $table->string('course_type')->nullable();
+            $table->string('expected_start_date')->nullable();
+            $table->string('course_of_interest_1');
+            $table->string('course_of_interest_2')->nullable();
+            $table->string('course_of_interest_3')->nullable();
+            $table->string('device_name')->nullable();
+            $table->string('enquiry')->nullable();
+            $table->boolean('is_emailed')->default(0);
+            $table->boolean('is_exported')->default(0);
+            $table->string('registered_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('applicants');
+    }
+}
