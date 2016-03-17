bc50e5b74dcb274f115bcc932a668957226a8a30
Park Lai <park8lai@gmail.com>
2016-03-17 18:26:20 +0800

    Login|Logout function

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,71 +2,52 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
-use Validator;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\ThrottlesLogins;
-use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Http\Request;
+use Sentinel;
+use Session;
+use App\User;
 
 class AuthController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Registration & Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
-    |
-    */
-
-    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
-
-    /**
-     * Where to redirect users after login / registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
-
-    /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    // POST
+    public function login(Request $request)
     {
-        $this->middleware('guest', ['except' => 'logout']);
+        $this->validate($request, [
+            'login' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('login', 'password');
+
+        try {
+
+            if ($user = Sentinel::authenticate($credentials, $request->get('remember_me', false))) {
+
+                Sentinel::login($user);
+                Session::flash('success', 'Good day, ' . $user . '.');
+
+            } else {
+
+                Session::flash('error', 'User not found or password wrong');
+                return back();
+
+            }
+
+        } catch (\Cartalyst\Sentinel\Checkpoints\NotActivatedException $e) {
+            Session::flash('error', $e->getMessage());
+            return back();
+        }
+
+        return redirect()->route('applicant');
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
+    // GET
+    public function logout()
     {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|confirmed|min:6',
-        ]);
-    }
+        Sentinel::logout();
+        Session::flash('success', 'Logout succesfully !');
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
+        return redirect()->route('home');
     }
 }
