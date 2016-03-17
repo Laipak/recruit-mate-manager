642225b8407cfdc5f7d775dfc6c3160a6fa7669e
Park Lai <park8lai@gmail.com>
2016-03-17 18:23:58 +0800

    Auth middleware built

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,29 +2,54 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Contracts\Auth\Guard;
 use Closure;
-use Illuminate\Support\Facades\Auth;
+use Sentinel;
+use Response;
+use Redirect;
 
 class Authenticate
 {
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param  Guard  $auth
+     * @return void
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next)
     {
-        if (Auth::guard($guard)->guest()) {
-            if ($request->ajax() || $request->wantsJson()) {
-                return response('Unauthorized.', 401);
+        if (!Sentinel::check()) {
+
+            if ($request->ajax()) {
+
+                return Response::json(['message' => 'Unauthorized'], 401);
+
             } else {
-                return redirect()->guest('login');
+
+                return Redirect::guest(route('home'));
+
             }
         }
-
+        
         return $next($request);
     }
 }
